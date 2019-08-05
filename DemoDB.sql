47495abdf66d5c39a2ae4da9447bcec0acb84e89
alinaer <51811640+alinaer@users.noreply.github.com>
2019-08-05 17:33:52 +0600

    Add files via upload

---
+++ DemoDB.sql
@@ -0,0 +1,10 @@
+SELECT * from requests where developerid=1
+
+ 
+--SELECT B.*
+--from DeveloperBacklogs DB, backlog B
+--where DB.BacklogId=b.Id and DB.DeveloperId=1
+
+--SELECT Product,  count(*) Количество_заявок
+--from Requests R 
+--group by Product
